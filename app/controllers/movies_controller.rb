76c9b653355cc888cc33e3a6cbe85bd64cd25289
Patrick Alarcon <palarcon@uncc.edu>
2017-03-23 15:15:41 +0000

    Part 2 Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,14 +9,24 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-  
-
 
   def index
-    @movies = Movie.all
-    if (params[:sort] == "title")
-      @movies.order!(title: :asc)
-    end
+		if params.key?(:sort_by)
+			session[:sort_by] = params[:sort_by]
+		elsif session.key?(:sort_by)
+			params[:sort_by] = session[:sort_by]
+			redirect_to movies_path(params) and return
+		end
+		@hilite = sort_by = session[:sort_by]
+		@all_ratings = Movie.all_ratings
+		if params.key?(:ratings)
+			session[:ratings] = params[:ratings]
+		elsif session.key?(:ratings)
+			params[:ratings] = session[:ratings]
+			redirect_to movies_path(params) and return
+		end
+		@checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
+    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
   end
 
   def new
